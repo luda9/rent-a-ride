998b5be44cb9e05af93a10bedc5ea3d5ce1e8a03
hiramramirez <hiram.j.ramirez@gmail.com>
2022-08-23 17:06:52 -0500

    car controller fixed and view added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,10 @@
 class Car < ApplicationRecord
-  validates :brand, :year, :color, :model, :plate, :price, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :model, presence: true
+  validates :plate, presence: true
+  validates :price, presence: true
   validates :year, :price, numericality: { only_integer: true }
   validates :year, numericality: { less_than_or_equal_to: 2022 }
 end
