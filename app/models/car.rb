622a2198c77a21cab7dc8c7e7216b8ab0c6cae2d
Daniel Santillan <ldms99@hotmail.com>
2022-08-25 16:17:27 -0500

    car show created and many car pictures working

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,11 +2,9 @@ class Car < ApplicationRecord
   validates :brand, :year, :color, :model, :plate, :price, presence: true
   validates :year, :price, numericality: { only_integer: true }
   validates :year, numericality: { less_than_or_equal_to: 2022 }
-  has_one_attached :photo
+  has_many_attached :photos
 
   has_many :bookings, dependent: :destroy
   # belongs_to :user
   # belongs_to :user, through: :bookings
 end
-
-# comentario
