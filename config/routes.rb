6c006f89c0afafb723ad0528c6e2d83d261caff8
hiramramirez <hiram.j.ramirez@gmail.com>
2022-08-26 15:23:55 -0500

    edit user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create edit update]
   end
   resources :bookings, only: %i[destroy index]
+
+  resources :users, only: :show
 end
